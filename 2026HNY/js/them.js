f3bb9365ea9efeecc900ea7d2d5b63e5e2ef8ded
hsuanpatty <106578679+hsuanpatty@users.noreply.github.com>
2026-05-18 09:53:41 +0800

    Add files via upload

---
+++ 2026HNY/js/them.js
@@ -0,0 +1,31 @@
+// header banner
+$(".banner-img").slick({
+  items: 1,
+  nav: false,
+  dots: false,
+  loop: false,
+  autoplay: true,
+   arrows: false,
+  autoplayTimeout:6000,
+  responsive: {
+    0: {
+      items: 1
+    }
+  }
+});
+
+// 櫻花
+$(".Cherry-banner").slick({
+  items: 1,
+  nav: false,
+  dots: false,
+  loop: false,
+  autoplay: true,
+   arrows: false,
+  autoplayTimeout:6000,
+  responsive: {
+    0: {
+      items: 1
+    }
+  }
+});
